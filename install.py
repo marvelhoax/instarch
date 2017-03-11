b958a7b568eb636b4cb40736f85fb57dc62a5eff
Marvel Hoax <marvelhoax@gmail.com>
2017-03-11 16:32:02 +0530

    Script updated

--- install.py
+++ install.py
@@ -28,11 +28,9 @@ def gen_fstab():
 	print "Its time to configure grub, This shit is not working and Allah is not helping me to figure out this problem"
 	answer = raw_input("\033[1;34mDo you want to configure grub?")
 	if answer == 'y' or answer == 'Y':
-		configure_grub
+		configure_grub()
 	else:
 		print "You are looser!Fck this bitch..."
-
-gen_fstab
 
 # chroot in to the new system
 def configure_grub():
@@ -41,167 +39,170 @@ def configure_grub():
 	call("arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg", shell=True)
 	print "\033[1;32m#####Grub Installed Successfully#############\033[0m"
 	
-	print " "
+print " "
 
-	# sleep for few seconds.
-	time.sleep(2)
-	#####################################################################
-	#                             BASE SETTING                          #
-	#####################################################################
+gen_fstab()
 
-	print "\033[40;1;31m Lets Configure Base Setting\033[0m"
+# sleep for few seconds.
+time.sleep(2)
+#####################################################################
+#                             BASE SETTING                          #
+#####################################################################
 
-	# set password for root
-	print "\033[31m Enter the root password\033[0m"
-	passwd = call("passwd", shell=True)
+print "\033[40;1;31m Lets Configure Base Setting\033[0m"
 
-	# set hostname
-	enter_hostname = raw_input("\033[32mEnter the hostname for your ARCH\033[0m")
-	conf_hostname = "echo ", enter_hostname, " > /etc/hostname"
-	hostname = call("arch-chroot /mnt "+conf_hostname, shell=True)
+# set password for root
+print "\033[31m Enter the root password\033[0m"
+passwd = call("passwd", shell=True)
 
-	# add new user & set password
-	get_username = raw_input( "\033[31m Lets add new user & set password\033[0m")
-	set_username = "useradd -m -G whell ",get_username
-	user_pass = "passwd ", get_username
-	user_passwd = call("arch-chroot /mnt "+user_pass, shell=True)
+# set hostname
+enter_hostname = raw_input("\033[32mEnter the hostname for your ARCH\033[0m")
+conf_hostname = "echo ", enter_hostname, " > /etc/hostname"
+hostname = call("arch-chroot /mnt "+conf_hostname, shell=True)
 
-	# edit sudoers to allow new user to sudo
-	file_sudoers = open("visudo", "w+")
-	uncomment = file_sudoers.readline()
+# add new user & set password
+get_username = raw_input( "\033[31m Lets add new user & set password\033[0m")
+set_username = "useradd -m -G whell ",get_username
+user_pass = "passwd ", get_username
+user_passwd = call("arch-chroot /mnt "+user_pass, shell=True)
+	
+# edit sudoers to allow new user to sudo
+file_sudoers = open("visudo", "w+")
+uncomment = file_sudoers.readline()
 	if uncomment == "#%wheel ALL=(ALL) ALL":
 		uncomment.replace("#%wheel", " %whell")
 
 	file_sudoers.close()
 
-	# check the name of your network interface
-	interface = call("ip link", shell=True)
+# check the name of your network interface
+interface = call("ip link", shell=True)
 
-	# enable aquiring dynamic IP
-	enable_dynamic_ip = call("systemctl enable dhcpcd.service", shell=True)
-	start_dynamic_ip = call("systemctl start dhcpcd.service", shell=True)
+# enable aquiring dynamic IP
+enable_dynamic_ip = call("systemctl enable dhcpcd.service", shell=True)
+start_dynamic_ip = call("systemctl start dhcpcd.service", shell=True)
 
-	# generate new locales & set one system wide
-	generate_locale = call("arch-chroot /mnt locale-gen", shell=True)
-	set_locale = call("arch-chroot /mnt localectl set-local LANG=en_US.UTF-8", shell=True)
+# generate new locales & set one system wide
+generate_locale = call("arch-chroot /mnt locale-gen", shell=True)
+set_locale = call("arch-chroot /mnt localectl set-local LANG=en_US.UTF-8", shell=True)
 
-	# select timezone and set it permanent
-	tzselect = call("arch-chroot /mnt tzselect", shell=True)
-	set_timezone = call("arch-chroot /mnt timedatectl set-timezone 'Europe/Berlin'", shell=True)
+# select timezone and set it permanent
+tzselect = call("arch-chroot /mnt tzselect", shell=True)
+set_timezone = call("arch-chroot /mnt timedatectl set-timezone 'Europe/Berlin'", shell=True)
 	
-	# set hardware clock and sync using ntp
-	hwclock = call("arch-chroot /mnt hwclock --systohc --utc", shell=True)
-	set_ntp = call("arch-chroot /mnt timedatectl set-ntp true", shell=True)
+# set hardware clock and sync using ntp
+hwclock = call("arch-chroot /mnt hwclock --systohc --utc", shell=True)
+set_ntp = call("arch-chroot /mnt timedatectl set-ntp true", shell=True)
 
-	# set hardware clock and sync using ntp
-	hwclock = call("arch-chroot /mnt hwclock --systohc --utc", shell=True)
-	set_ntp = call("arch-chroot /mnt timedatectl set-ntp true", shell=True)
+# set hardware clock and sync using ntp
+hwclock = call("arch-chroot /mnt hwclock --systohc --utc", shell=True)
+set_ntp = call("arch-chroot /mnt timedatectl set-ntp true", shell=True)
 
-	################# BASIC SETTINGS OVER #################################
+################# BASIC SETTINGS OVER #################################
 
-	time.sleep(5)
+time.sleep(5)
 
-	#######################################################################
-	#                       DESKTOP ENVIRONMENT                           #
-	#######################################################################
+#######################################################################
+#                       DESKTOP ENVIRONMENT                           #
+#######################################################################
 
-	print "\033[36m Installing Desktop Environment\033[0m"
+print "\033[36m Installing Desktop Environment\033[0m"
 
-	print "1.AMD\n 2. INTEL\n 3.  Nvdia"
-	Video_hardware = raw_input("select the Brand for video driver")
+print "1.AMD\n 2. INTEL\n 3.  Nvdia"
+Video_hardware = raw_input("select the Brand for video driver")
 
-	if video_hardware == "1":
-		# install the default video driver
-		default_driver = call("arch-chroot /mnt pacman -S xf86-video-amdgpu", shell=True)
-		if default-driver == "0":
-			print "Video Driver installed successfully"
-		else :
-			print "Error in installing Video Driver. Try Again"
-	elif video_hardware == "2":
-		default_driver = call("arch-chroot /mnt pacman -S xf86-video-intel", shell=True)
-		if default-driver == "0":
-			print "Video Driver installed successfully"
-		else :
-			print "Error in installing Video Driver. Try Again"
-	elif video_hardware == "3":
-		default_hardware = call("arch-chroot /mnt pacman -S xf86-video-nouveau", shell=True)
-		if default-driver == "0":
-			print "Video Driver installed successfully"
-		else :
-			print "Error in installing Video Driver. Try Again"
-	else:
-		print "Error in installing video driver"
-
-	# install OpenGL support
-	install_mesa = call("arch-chroot /mnt pacman -S mesa", shell=True)
-
-	# install video driver specific for your hardware
-	install_video_driver = call("arch-chroot /mnt pacman -S xf86-video-intel", shell=True)
-
-	# install Xorg packages
-	xorg_packages = call("arch-chroot /mnt pacman -S xorg-server xorg-utils xorg-xinit xterm",shell=True)
-	
-	# fancy loading ^-^
-	time.sleep(1)
-	print "loading",
-	for x in range(1,5):
-		print "\033[36m .\033[0m",
-		print ""
-
-	# installing Desktop Environment of your choice
-	print "Choose the Desktop Environment to Install for Your Arch"
-	print "1. Gnome\n 2. KDE\n 3. Xfce4\n 4. Lxde\n  5. Cinnamon\n 6. MATE\n 7. LXQt"
-	desktop = raw_input("Enter the number of desktop environment? ")
-	if desktop == "1":
-		install_de = call("arch-chroot /mnt pacman -S gnome gnome-extra", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "2":
-		install_de = call("arch-chroot /mnt pacman -S kde kde-applications", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "3":
-		install_de = call("arch-chroot /mnt pacman -S xfce4 xfce4-goodies", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "4":
-		install_de = call("arch-chroot /mnt pacman -S lxde", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "5":
-		install_de = call("arch-chroot /mnt pacman -S cinnamon", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "6":
-		install_de = call("arch-chroot /mnt pacman -S mate", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
-	elif desktop == "7":
-		install_de = call("arch-chroot /mnt pacman -S lxqt", shell=True)
-		if install_de == "0":
-			print "Desktop installed Successfully"
-		else:
-			print "Failed to install Desktop Environment"
+if video_hardware == "1":
+	# install the default video driver
+	default_driver = call("arch-chroot /mnt pacman -S xf86-video-amdgpu", shell=True)
+	if default-driver == "0":
+		print "Video Driver installed successfully"
 	else :
+		print "Error in installing Video Driver. Try Again"
+elif video_hardware == "2":
+	default_driver = call("arch-chroot /mnt pacman -S xf86-video-intel", shell=True)
+	if default-driver == "0":
+		print "Video Driver installed successfully"
+	else :
+		print "Error in installing Video Driver. Try Again"
+elif video_hardware == "3":
+	default_hardware = call("arch-chroot /mnt pacman -S xf86-video-nouveau", shell=True)
+	if default-driver == "0":
+		print "Video Driver installed successfully"
+	else :
+		print "Error in installing Video Driver. Try Again"
+else:
+	print "Error in installing video driver"
+
+# install OpenGL support
+install_mesa = call("arch-chroot /mnt pacman -S mesa", shell=True)
+
+# install video driver specific for your hardware
+install_video_driver = call("arch-chroot /mnt pacman -S xf86-video-intel", shell=True)
+
+# install Xorg packages
+xorg_packages = call("arch-chroot /mnt pacman -S xorg-server xorg-utils xorg-xinit xterm",shell=True)
+	
+# fancy loading ^-^
+time.sleep(1)
+print "loading",
+for x in range(1,5):
+	print "\033[36m .\033[0m",
+	
+print ""
+
+# installing Desktop Environment of your choice
+print "Choose the Desktop Environment to Install for Your Arch"
+print "1. Gnome\n 2. KDE\n 3. Xfce4\n 4. Lxde\n  5. Cinnamon\n 6. MATE\n 7. LXQt"
+desktop = raw_input("Enter the number of desktop environment? ")
+if desktop == "1":
+	install_de = call("arch-chroot /mnt pacman -S gnome gnome-extra", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "2":
+	install_de = call("arch-chroot /mnt pacman -S kde kde-applications", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "3":
+	install_de = call("arch-chroot /mnt pacman -S xfce4 xfce4-goodies", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "4":
+	install_de = call("arch-chroot /mnt pacman -S lxde", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "5":
+	install_de = call("arch-chroot /mnt pacman -S cinnamon", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "6":
+	install_de = call("arch-chroot /mnt pacman -S mate", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+elif desktop == "7":
+	install_de = call("arch-chroot /mnt pacman -S lxqt", shell=True)
+	if install_de == "0":
+		print "Desktop installed Successfully"
+	else:
+		print "Failed to install Desktop Environment"
+else :
 		print "Error in installing Desktop Environment"
 
-	# install & enable login manager lightdm
-	install_lightdm = call("arch-chroot /mnt pacman -S lightdm-gtk-greeter", shell=True)
-	start_lightdm = call("arch-chroot /mnt systemctl enable lightdm.service", shell=True)
+# install & enable login manager lightdm
+install_lightdm = call("arch-chroot /mnt pacman -S lightdm-gtk-greeter", shell=True)
+start_lightdm = call("arch-chroot /mnt systemctl enable lightdm.service", shell=True)
 
-	############### DESKTOP ENVIROMENT INSTALLED #################
+############### DESKTOP ENVIROMENT INSTALLED #################
 
 
 print "Volla! Arch is ready for customization. Just type reboot and start using Arch Linux"
